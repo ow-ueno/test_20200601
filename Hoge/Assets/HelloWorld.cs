51441b4ae37474ac40d22051eb8203d80dc3c2a8
ow-ueno <66234056+ow-ueno@users.noreply.github.com>
2020-06-01 17:32:41 +0900

    玉転がし_2006011725
    玉を作りました。玉が操作で動くようになりました。
    床を作りました。
    カメラが不要な回転をしなくなりました。
    
    壁を作りました。
    回収対象オブジェクトを設置しました。
    回収対象オブジェクトが回転するようになりました。

---
+++ Hoge/Assets/HelloWorld.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    int myInt = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+
+        myInt = 22;
+        Debug.Log(MultiplyByTwo(myInt));
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
